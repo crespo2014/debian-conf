3c77019402260ce67289dea3693111780f8923cc
Lester <lester.crespo.es@gmail.com>
2009-06-04 00:44:35 +0100

    updated

---
+++ etc/insserv/overrides/mountdevsubfs.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+### BEGIN INIT INFO
+# Provides:          mountdevsubfs
+# Required-Start:    mountkernfs
+# Required-Stop:
+# Should-Start:      udev
+# Default-Start:     1 2 3 4 5 
+# Default-Stop:
+# Short-Description: Mount special file systems under /dev.
+# Description:       Mount the virtual filesystems the kernel provides
+#                    that ordinarily live under the /dev filesystem.
+### END INIT INFO
