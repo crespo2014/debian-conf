020dcd1269a0843bb4cdf426295bbd25f3c18ac1
Lester <lester.crespo.ie@gmail.com>
2015-05-08 11:26:26 +0100

    added dependencies graph builder

---
+++ root/build-depends.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "digraph {  concentrate=true; rankdir=LR; node [fontsize=10]; node [nodesep=0.75]; node [ranksep=0.75]; "
+echo " node [shape=box]; edge [weight=1.2]; "
+#    node [color=none];
+#    graph [bb="0,0,1000,1000"];
+#graph[size="100,100"]; 
+# graph [ratio=0.5];
+
+cd /etc/init.d
+grep Required-Start * | cut -d ':' -f 1,3 | \
+while read line
+do
+node=$(echo $line | cut -d ':' -f 1)
+for parent in $(echo $line | cut -d ':' -f 2)
+do
+ echo " \"$parent\" -> \"$node\""
+done
+done 
+
+echo "}"
