0627f4ebb2516b915eab1014299ccc9f0b61c368
Lester <lester.crespo.es@gmail.com>
2015-05-12 11:40:50 +0100

    Added synchronization

--- etc/init.d/speedup.sh
+++ etc/init.d/speedup.sh
@@ -16,28 +16,37 @@ function init_1() {
   SCRIPTS="hostname.sh mountkernfs.sh udev keyboard-setup mountdevsubfs.sh hdparm hwclock.sh checkroot.sh checkroot-bootclean.sh kbd console-setup alsa-utils pppd-dns procps udev-mtab urandom x11-common networking rpcbind mountnfs.sh mountnfs-bootclean.sh bootmisc.sh motd bootlogs single"
   for script in $SCRIPTS
   do
-    /etc/init.d/$script start
+    /etc/init.d/$script start &>>/var/log/speedup.log
   done
-  cat /proc/deferred_initcalls &
+  cat /proc/deferred_initcalls &>>/var/log/speedup.log &
 }
 
 function init_2() {
   mount -t proc proc /proc "-onodev,noexec,nosuid"
   mount -t sysfs sys /sys "-onodev,noexec,nosuid"
   cat /proc/deferred_initcalls &> /dev/null &
-  SCRIPTS="hostname.sh early-readahead udev& mountall.sh mountdevsubfs.sh dbus& slim stop-readahead-fedora acct acpid atd cron dbus exim4 motd rsync bootlogs network-manager saned rpcbind rc.local rmnologin " 
+  SCRIPTS="hostname.sh early-readahead udev& mountall.sh mountdevsubfs.sh dbus& slim W stop-readahead-fedora acct acpid atd cron dbus exim4 motd rsync bootlogs network-manager saned rpcbind rc.local rmnologin " 
+# using W to wait for all background to finish  
+  pid=
   for script in $SCRIPTS
   do
-    cmd=${script::-1}
     cmd_end=${script: -1}
-    if [ "$cmd_end" != "&" ]; then
-      cmd=$cmd$cmd_end
-      cmd_end=
-    fi 
-    /etc/init.d/$cmd start $cmd_end
+	if [ "$script" = "W" ]; then
+	  for id in $pid
+	  do
+	    wait $id
+	  done
+	  pid=
+	else	
+      if [ "$cmd_end" != "&" ]; then
+	    /etc/init.d/$script start &>>/var/log/speedup.log
+	  else
+	    /etc/init.d/${script::-1} start &>>/var/log/speedup.log &
+	    pid="$pid $!"
+      fi 
+	fi
   done
 }
-echo "Starting .... $SCRIPTS"
 
 case "$1" in
   start|"")
