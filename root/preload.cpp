6788ecf8690e9f589c280770f6461ef3f9523066
Lester <lester.crespo.es@gmail.com>
2015-06-15 22:23:23 +0100

    Starting read ahead

---
+++ root/preload.cpp
@@ -0,0 +1,22 @@
+/*
+ * preload.cpp
+ *
+ *  Created on: 15 Jun 2015
+ *      Author: lester
+ *
+ *  Read ahead a list files stored in a file.
+ *  Try to order reads base on physical position on disk.
+ *  Check if squeduler can be change to be more agresive and not allow multiple operations
+ *  using noop scheduler seam to be a good option
+ *
+ * cat /sys/block/{DEVICE-NAME}/queue/scheduler
+ * cat /sys/block/sda/queue/scheduler
+ * echo {SCHEDULER-NAME} > /sys/block/{DEVICE-NAME}/queue/scheduler
+ * echo noop > /sys/block/hda/queue/scheduler
+ *
+ * kernel cmd line elevator=noop
+ */
+
+
+
+
