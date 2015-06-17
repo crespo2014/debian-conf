c6e13b874855abc03e3108dcc8c12ae825a0fba0
Lester <lester.crespo.es@gmail.com>
2015-06-17 23:57:50 +0100

    Deferred was not called when boot in single level

--- etc/init.d/deferred_init.sh
+++ etc/init.d/deferred_init.sh
@@ -1,10 +1,10 @@
-#! /bin/sh
+#! /bin/bash
 ### BEGIN INIT INFO
 # Provides:			modules          
 # Required-Start:       $all		
 # Required-Stop:
 # Should-Start:      
-# Default-Start:	1 2 3 4 5  
+# Default-Start:	S 1 2 3 4 5  
 # Default-Stop:
 # Short-Description: 
 # Description:       
@@ -14,9 +14,12 @@ PATH=/sbin:/bin
 
 case "$1" in
   start|"")
-
-	cat /proc/deferred_initcalls
-        udevadm trigger
+    single=$(grep -wo single /proc/cmdline)
+    if [ "$runlevel" == "S" ] && [ "$single" == "" ]; then
+    	exit
+    fi
+    cat /proc/deferred_initcalls
+    udevadm trigger
 	;;
   restart|reload|force-reload)
 	;;
